64089d121696bf7fd6e72e600966c1de9b51a1c6
Andre <dego89@gmx.net>
2015-03-19 12:41:20 -0500

    Initial Commit/ changed colors and clean up into classes.

--- Main.java
+++ Main.java
@@ -4,8 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+
 
 
 //TODO have paddle speed affect ball's direction
